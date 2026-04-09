223f3aad0ba836e324204ef31cf9baa4b95fff70
ziomidlo <kziomek33@gmail.com>
2026-04-09 15:57:25 +0200

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/example/InsuranceFeeTracker/InsuranceFeeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InsuranceFeeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceFeeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
